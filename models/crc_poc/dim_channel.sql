d18398c0c6d29a0151d53a09e11ad07572d23ea0
phlay@deloitte.com <phlay@deloitte.com>
2022-05-19 08:44:06 +0000

    dsafsafasf

--- models/crc_poc/dim_channel.sql
+++ models/crc_poc/dim_channel.sql
@@ -3,7 +3,7 @@
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
-asfasfsafsafasf a safsaefsadfasdfsf
+asfasfsafsafasf a safsaefsadfasdfsfdadASDAsdASDAsdasdqasd
     Try changing "table" to "view" below fffff abc abdccsdfsfsf
 */
 
